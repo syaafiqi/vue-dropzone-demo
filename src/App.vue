90f8289e16d8882a9f835e169526e72d3f0b5e0c
Muhammad Syafiq Mustofa <syaafiqi@yahoo.com>
2022-01-14 15:47:30 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"><img width="15%" src="./assets/logo.png" /> <Attachment /></div>
+</template>
+
+<script>
+import Attachment from "./components/Attachment";
+
+export default {
+  name: "App",
+  components: {
+    Attachment
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
